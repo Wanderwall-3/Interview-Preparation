cd8a718a5bfc1f10b3b65e23f374c7430a49d9bf
wanderwall-3 <wanderwall420@gmail.com>
2025-09-09 00:32:52 +0530

    merge sort upgrade

--- AllSortingAlgorithm.java
+++ AllSortingAlgorithm.java
@@ -1,4 +1,3 @@
-package InterviewPreparation;
 import java.util.*;
 
 public class AllSortingAlgorithm {
@@ -19,6 +18,18 @@ public class AllSortingAlgorithm {
             arr[pos] = tem;
         }
     }
+
+    public static void insertionSort(int[] arr, int size) {
+        for(int i=1; i<size; i++) {
+            for(int j=i-1; j>=0; j--){
+                if(arr[j]>arr[j+1]){
+                    int tem = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j]=tem;
+                }
+            }
+        }
+    }
  
     public static void quickSort(int[] arr, int start, int end) {           //Reference: https://youtu.be/WprjBK0p6rw?si=x0DxBDkvp5qB24rK
         if(start >= end) return;
@@ -26,6 +37,7 @@ public class AllSortingAlgorithm {
         int j = start-1;
         for(int i=start; i < end; i++) {
             if(arr[i]<=arr[pivotPos]) {
+                System.out.println(arr[i]+" "+arr[j]);
                 j++;
                 int tem = arr[i];
                 arr[i] = arr[j];
@@ -42,22 +54,44 @@ public class AllSortingAlgorithm {
         quickSort(arr, pivotPos+1, end);
     }
 
-    public static void insertionSort(int[] arr, int size) {
-        for(int i=1; i <= size; i++) {
-            int pos = i;
-            for(int j=i-1; j>=0; j--) {
-                if(arr[pos] <= arr[j]) {
-                    int tem = arr[pos];
-                    arr[pos] = arr[j];
-                    arr[j] = tem;
-                    pos = j;
-                }
+    public static void mergeSort(int[] arr,int start, int end, int size) {
+        // if(start >= end) return;
+        // i++;
+        // if(i == 5) return;
+        int mid = start + ((end-start)/2);
+        if(start<end){
+            mergeSort(arr, start, mid, size);
+            mergeSort(arr, mid+1, end, size);
+        }
+        int leftArraySize = mid - start;
+        int rightArraySize = end - mid;
+
+        int[] l = new int[leftArraySize];
+        int[] r = new int[rightArraySize];
+
+        for(int i = 0; i<leftArraySize; i++) {
+            l[i] = arr[start+i];
+        }
+        for(int i =0; i<rightArraySize; i++) {
+            r[i] = arr[mid+1+i];
+        }
+
+        int i=0,j=0;
+        int ind = start;
+        while(i<leftArraySize && j<rightArraySize) {
+            if(l[i]<=r[j]){
+                arr[ind++]=l[i++];
+            }
+            else{
+                arr[ind++] = r[j++];
             }
         }
-        System.out.println("Insertion sort Process complete");
-    }
-
-    public static void mergeSort(int[] arr, int size) {
+        while(i<leftArraySize){
+            arr[ind++] = l[i++];
+        }
+        while (j<rightArraySize) {
+            arr[ind++] = r[j++];
+        }
 
     }
 
@@ -69,7 +103,7 @@ public class AllSortingAlgorithm {
         String[] str = sc.nextLine().split(" ");
         int ind = 0;
         for(String i : str) arr[ind++] = Integer.parseInt(i);
-        quickSort(arr,0,size-1);
+        mergeSort(arr,0,size-1,size);
         System.out.println(Arrays.toString(arr));
         sc.close();
     }
